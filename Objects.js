c71786adb7c778adc7914930b32c3fdb1393f3e8
MoonNoah <moon.503@buckeyemail.osu.edu>
2022-07-07 19:18:02 -0400

    added num button functionality

--- Objects.js
+++ Objects.js
@@ -1,9 +1,1 @@
 //Created 7/7/2022 by Noah Moon
-const Form = document.getElementById("calculator");
-const Screen = Form.screen;
-
-function printToScreen(text){
-    Screen.innerHTML = text
-}
-
-printToScreen("THIS IS A TEST")
