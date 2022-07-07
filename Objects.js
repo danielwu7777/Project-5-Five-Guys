9225659df91cf7b047941ee8502cc608c8901966
danielwu3412 <wu.3412@buckeyemail.osu.edu>
2022-07-07 19:13:39 -0400

    implemented arithmetic functions and added test file for functions

--- Objects.js
+++ Objects.js
@@ -1,9 +1,34 @@
 //Created 7/7/2022 by Noah Moon
+// Edited 7/7/2022 by Daniel Wu: Added functions for arithmetic operations
 const Form = document.getElementById("calculator");
 const Screen = Form.screen;
 
 function printToScreen(text){
     Screen.innerHTML = text
+}
+
+// Created 7/7/2022 by Daniel Wu
+// Takes two integers and returns the sum
+function sum(x, y) {
+    return x + y;
+}
+
+// Created 7/7/2022 by Daniel Wu
+// Takes two integers and returns the first input minus the second
+function difference(left, right) {
+    return left - right;
+}
+
+// Created 7/7/2022 by Daniel Wu
+// Takes two integers and returns the product
+function product(x, y) {
+    return x * y;
+}
+
+// Created 7/7/2022 by Daniel Wu
+// Takes two integers and returns the first input divided by the second
+function quotient(x, y) {
+    return x / y;
 }
 
 printToScreen("THIS IS A TEST")
